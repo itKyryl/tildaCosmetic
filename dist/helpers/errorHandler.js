1f32079bd8f38b25f8ff031faa97d746d4f3e144
itKyryl <it.kyryl@gmail.com>
2022-12-24 01:15:14 +0100

    Error handler fix

--- dist/helpers/errorHandler.js
+++ dist/helpers/errorHandler.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function errorHandler(error, _, res) {
+function errorHandler(error, _, res, next) {
     const errorMessage = error.message || "Error! Something went wrong on the server.";
     res.status(501).send(errorMessage);
 }
